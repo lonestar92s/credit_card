160257b7a6a688047930533ba8de51698a4308d7
Andrew Aluko <aluko17@icloud.com>
2019-05-02 15:53:59 -0500

    updates

--- creditCard.js
+++ creditCard.js
@@ -1,26 +1,55 @@
+//purchase timestamp function
 
+const timestamp = ()=> { 
+	new Date().valueOf();
+}
 
-
-//Class to create new credit card account with unique properties
+//Class to create new credit card account with unique properties and methods
 
 class Account {
 	constructor(name){
 		this.name = name
-		this.date = Date()
+		this.accountAge = 0
+		this.balance = 0
 		//apr
 	}
-	creditLimit(amount){
-		this.creditLimit = amount
+	creditLimit(limit){
+		if(limit <= 0){
+			console.log("This account is not active")
+		}	
+		else
+			this.creditLimit = limit
 	}
+	purchase(amount, item, merchant){
+		console.log("Welcome, " + this.name + '! Your credit limit is ' + this.creditLimit + ' dollars.')
+		this.balance += amount;
+		//refactor to clean up?
+		console.log('You purchased ' + item + ' from ' + merchant + ' for ' + amount + ' dollars.')
+		console.log('Your balance is ' + this.balance + ' dollars')
+	}
+	payment(amount){
+		this.balance -= amount
+	} 
 }
+			 
 
 
-//instantiate new Account
+//logic for APR
+
+
+//Create new Account
 let andrewAccount = new Account('Andrew')
+let christyAccount = new Account('Christy')
 
 
 //user input credit limit
-andrewAccount.creditLimit(5000)
+andrewAccount.creditLimit(5)
+andrewAccount.purchase(10, 'cookies', 'HEB')
 
-console.log(andrewAccount)
+christyAccount.creditLimit(500)
+
+
+
+
+
 
