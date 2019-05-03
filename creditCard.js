0a026e2c2bb65db5cb3d6678c07fd4d98649e05f
Andrew Aluko <aluko17@icloud.com>
2019-05-03 16:16:48 -0500

    apr formula

--- creditCard.js
+++ creditCard.js
@@ -8,38 +8,46 @@ const timestamp = ()=> {
 //Constructor to create new credit card account with unique properties and methods
 
 class Account {
-	constructor(name){
-		this.name = name
-		this.accountAge = 0
-		this.balance = 0
-		this.items = []
-		//apr
-	}
-	creditLimit(limit){
+	constructor(name, apr, limit){
+		this.name = name;
+		this.apr = apr
 		this.creditLimit = limit
+		//Account age by days
+		this.accountAge = 0;
+		this.balance = 0;
+		this.purchases = [];
+		this.payments = []
+		this.time = []
 	}
-	purchase(amount, item, merchant){
+	
+	purchase(amount, dayOfMonth, item, merchant){
 		if(this.creditLimit > 0){
-		console.log("Welcome, " + this.name + '! Your credit limit is ' + this.creditLimit + ' dollars.')
-		
+		console.log("Welcome, " + this.name + '! Your credit limit is $' + Number(this.creditLimit).toLocaleString() + ' dollars, with an APR of ' +this.apr + '%.')
 		this.balance += amount;
-		//keeps track of individual customers items purchased 
-		this.items.push(amount, item, merchant)
+		//this.time.push(new Date().toString())
+		
+		//keeps track of individual customers items purchased
+		this.purchases.push('$'+ amount, item, merchant)
 		//refactor to clean up?
-		console.log('You purchased ' + item + ' from ' + merchant + ' for ' + amount + ' dollars.')
-		console.log('Your balance is ' + this.balance + ' dollars')
+		console.log('You purchased ' + item + ' from ' + merchant + ' for $' + amount + ' dollars.')
+		console.log('Your avalaible credit is $' + Number((this.creditLimit - amount)).toLocaleString() + '.')
 		}
 		else(console.error("Hi " + this.name + ", this account is currently not active."))
 	}
-	payment(amount){
+	payment(amount, dayOfMonth){
+		if(amount > 0){
 		this.balance -= amount
+		this.payments.push('$' + amount)
+		console.log("Hi " + this.name + ", you have made a payment of $" + amount + " dollars. Your available credit is now $" + Number((this.creditLimit - this.balance + amount)).toLocaleString() + '.')
+		 }
+		 else(console.error("Payment must be more than 0 dollars"))
 	}
-	//timer function for practicality 
-	//1 minute equals 1 day
-	timer(){
-		
+	checkBalance(){
+		console.log("Your account balance is $" + this.balance + ".")
+	}
+	accountDays(days){
 
-	} 
+	}
 }
 			 
 
@@ -47,22 +55,19 @@ class Account {
 //logic for APR
 
 
-//Create new Account
-let andrewAccount = new Account('Andrew')
-let christyAccount = new Account('Christy')
-let mackAccount = new Account('Mack')
+//Open new account with name, APR% and credit limit
+let andrewAccount = new Account('Andrew', 35, 10000)
+
+andrewAccount.purchase(500, 'Samsung TV', 1, 'Best Buy')
+andrewAccount.checkBalance()
 
 
-//user input credit limit
-// andrewAccount.creditLimit(1000)
-// andrewAccount.purchase(10, 'cookies', 'HEB')
 
-// christyAccount.creditLimit(500)
-// christyAccount.purchase(98, 'tread shoes', 'everlane')
 
-mackAccount.creditLimit(1000)
-mackAccount.purchase(30, 'food', 'blue buffalo')
-console.log(christyAccount.items)
+
+
+
+		
 
 
 
