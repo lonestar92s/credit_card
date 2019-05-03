65b3cf650d38b2f7630d439bb2fad3a07c5c1150
Andrew Aluko <aluko17@icloud.com>
2019-05-03 09:33:30 -0500

    individual items

--- creditCard.js
+++ creditCard.js
@@ -4,31 +4,41 @@ const timestamp = ()=> {
 	new Date().valueOf();
 }
 
-//Class to create new credit card account with unique properties and methods
+
+//Constructor to create new credit card account with unique properties and methods
 
 class Account {
 	constructor(name){
 		this.name = name
 		this.accountAge = 0
 		this.balance = 0
+		this.items = []
 		//apr
 	}
 	creditLimit(limit){
-		if(limit <= 0){
-			console.log("This account is not active")
-		}	
-		else
-			this.creditLimit = limit
+		this.creditLimit = limit
 	}
 	purchase(amount, item, merchant){
+		if(this.creditLimit > 0){
 		console.log("Welcome, " + this.name + '! Your credit limit is ' + this.creditLimit + ' dollars.')
+		
 		this.balance += amount;
+		//keeps track of individual customers items purchased 
+		this.items.push(amount, item, merchant)
 		//refactor to clean up?
 		console.log('You purchased ' + item + ' from ' + merchant + ' for ' + amount + ' dollars.')
 		console.log('Your balance is ' + this.balance + ' dollars')
+		}
+		else(console.error("Hi " + this.name + ", this account is currently not active."))
 	}
 	payment(amount){
 		this.balance -= amount
+	}
+	//timer function for practicality 
+	//1 minute equals 1 day
+	timer(){
+		
+
 	} 
 }
 			 
@@ -40,13 +50,19 @@ class Account {
 //Create new Account
 let andrewAccount = new Account('Andrew')
 let christyAccount = new Account('Christy')
+let mackAccount = new Account('Mack')
 
 
 //user input credit limit
-andrewAccount.creditLimit(5)
-andrewAccount.purchase(10, 'cookies', 'HEB')
+// andrewAccount.creditLimit(1000)
+// andrewAccount.purchase(10, 'cookies', 'HEB')
 
-christyAccount.creditLimit(500)
+// christyAccount.creditLimit(500)
+// christyAccount.purchase(98, 'tread shoes', 'everlane')
+
+mackAccount.creditLimit(1000)
+mackAccount.purchase(30, 'food', 'blue buffalo')
+console.log(christyAccount.items)
 
 
 
